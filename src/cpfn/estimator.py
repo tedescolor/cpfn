Fix sign error in exponent calculation

src/cpfn/estimator.py
@@ -94,7 +94,7 @@ class CPFN(nn.Module):
         eps = self.eps()
         zs = residuals / eps
         rs = zs.pow(2).sum(dim=-1)
-        exponents = rs - -0.5 * self.q * math.log(2.0 * math.pi) - torch.log(eps).sum() - math.log(m) - math.log(delta)
+        exponents = rs - 0.5 * self.q * math.log(2.0 * math.pi) - torch.log(eps).sum() - math.log(m) - math.log(delta)
         shape = list(exponents.shape)
         shape[1] = 1
         return torch.logsumexp(torch.cat([exponents, torch.zeros(*shape, device = xs.device)], dim=1), dim=1) + math.log(delta)        