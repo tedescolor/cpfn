afee710e57d0d7204b198eda10c8042d30518156
tedescolor <tedescolor@gmail.com>
2026-03-02 09:44:49 +0100

    Add verbose option to fit method in estimator

--- src/cpfn/estimator.py
+++ src/cpfn/estimator.py
@@ -208,7 +208,7 @@ class CPFN(nn.Module):
             device = self.eps().device
             return self.sample_conditional(torch.tensor(x, device=device, dtype=torch.float32), num_samples = num_samples, seed = seed).cpu().numpy()
 
-    def fit(self, xs: torch.Tensor, ys: torch.Tensor, epochs: int = 1000, lr: float = 1e-3, m: int = 30, h0: float = 5e-2, val_split: float = 0.0, batch_size: int = 256):
+    def fit(self, xs: torch.Tensor, ys: torch.Tensor, epochs: int = 1000, lr: float = 1e-3, m: int = 30, h0: float = 5e-2, val_split: float = 0.0, batch_size: int = 256, verbose: bool = True):
         if isinstance(xs, torch.Tensor) and isinstance(ys, torch.Tensor):
             self._istraining = True
             device = xs.device
@@ -238,7 +238,7 @@ class CPFN(nn.Module):
                 except: pass
     
             opt = torch.optim.Adam([p for p in self.parameters() if p.requires_grad], lr=lr)
-            pbar = tqdm(range(epochs), desc="Training CPFN")
+            pbar = tqdm(range(epochs), desc="Training CPFN", disable=not verbose)
             n_train = xt.shape[0]
     
             for epoch in pbar:
